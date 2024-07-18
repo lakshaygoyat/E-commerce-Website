84f7dc67d01dd98e9f3e2b8255fc793e72c86c47
roy7077 <raysagar807@gmail.com>
2024-07-19 04:40:40 +0530

    added

--- server/Controllers/Razorpay.js
+++ server/Controllers/Razorpay.js
@@ -82,7 +82,7 @@ exports.paymentVerification = async (req, res) => {
         });
 
         console.log("Payment is verified successfully");
-        res.redirect(`https://payment-gateway-lilac.vercel.app/paymentsuccess?reference=${razorpay_payment_id}`);
+        res.redirect(`http://localhost:8080/paymentsuccess?reference=${razorpay_payment_id}`);
     }
     catch(error)
     {
